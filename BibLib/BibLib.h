4eea2542a5d26d8ff4ee3e6a89e10cb48cedd1dd
Alexander Scheurer <alexander.scheurer@hs-furtwangen.de>
2012-11-08 11:28:14 +0100

    Revert "Some stuff"
    This reverts commit b3bff34289efebee82abf9e72a1b5ad1bc767383.

--- BibLib/BibLib.h
+++
@@ -1,8 +0,0 @@
-void charBlock(int x, int y, char c);
-void charLinePos(int x, int y, int r, char c);
-void gotoxy(int x,int y);
-void setColor(int farbe);
-void zeichne_stein(int x, int y, int laenge, int breite, char zeichen, int farbe);
-void ausgeben_feld();
-void clear_feld();
-void clrscr();
